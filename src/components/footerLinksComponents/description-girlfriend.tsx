e1d4e444a238df7d83d49cdfeb7325d7599407eb
Ali Haider <yourbro995@gmail.com>
2025-07-03 11:40:07 +0500

    fix description girlfriend

--- src/components/footerLinksComponents/description-girlfriend.tsx
+++ src/components/footerLinksComponents/description-girlfriend.tsx
@@ -2,26 +2,12 @@ import React from "react";
 
 function DescriptionGirlfriend() {
   return (
-    <p
-      className="text-base text-gray-400 mb-8 max-w-2xl mx-auto hidden md:block"
-      d
-    >
-      Our{" "}
-      <strong className="text-primary-light" d>
-        girlfriend
-      </strong>{" "}
-      photos tool uses advanced{" "}
-      <strong className="text-primary-light" d>
-        deepnude
-      </strong>{" "}
+    <p className="text-base text-gray-400 mb-8 max-w-2xl mx-auto hidden md:block">
+      Our <strong className="text-primary-light">girlfriend</strong> photos tool
+      uses advanced <strong className="text-primary-light">deepnude</strong>{" "}
       technology to create realistic images instantly. Get the best{" "}
-      <strong className="text-primary-light" d>
-        ai
-      </strong>{" "}
-      results with our cutting-edge{" "}
-      <strong className="text-primary-light" d>
-        realistic
-      </strong>{" "}
+      <strong className="text-primary-light">ai</strong> results with our
+      cutting-edge <strong className="text-primary-light">realistic</strong>{" "}
       generator.
     </p>
   );
